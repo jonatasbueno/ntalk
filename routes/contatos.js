675b54563df7b374bdcef93d4dc536bbf7559afd
Jônatas Livramento <jonataslivramento@MacBook-Pro-Bueno.local>
2020-10-01 03:28:30 -0300

    Corrigido os nomes da rotas

--- routes/contatos.js
+++ routes/contatos.js
@@ -4,9 +4,9 @@ module.exports = app => {
   const { contatos } = app.controllers
 
   app.get('/contatos', autenticar, contatos.index)
-  app.get('/contatos/:id', autenticar, contatos.show)
+  app.get('/contato/:id', autenticar, contatos.show)
   app.post('/contato', autenticar, contatos.create)
-  app.get('/contato/:id/editar', autenticar, contatos.edit)
+  app.get('/contato/:id/edit', autenticar, contatos.edit)
   app.put('/contato/:id', autenticar, contatos.update)
   app.delete('/contato/:id', autenticar, contatos.destroy)
 }
